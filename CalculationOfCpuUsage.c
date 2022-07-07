afe6f1a1343565a59e049f952beb7118e53eec60
im-stanly <77678060+im-stanly@users.noreply.github.com>
2022-07-07 21:34:44 +0200

    getCpuTime( ) added
    getCpuTime( ) method to get data from cpu

--- CalculationOfCpuUsage.c
+++ CalculationOfCpuUsage.c
@@ -16,6 +16,32 @@ typedef struct CpuData
     unsigned long long guest_nice;
 }CpuData;
 
+void getCpuTime(CpuData *cpuData) {
+    FILE* file = fopen("/proc/stat", "r");
+    if (file == NULL) {
+        perror("There was a problem opening the file");
+        return 0;
+    }
+
+    char buffer[1024];
+
+    char* ret = fgets(buffer, sizeof(buffer) - 1, file);
+    if (ret == NULL) {
+        perror("There was a problem starting to read /proc/stat");
+        fclose(file);
+        return;
+    }
+    fclose(file);
+
+    sscanf(buffer,
+        "cpu  %16llu %16llu %16llu %16llu %16llu %16llu %16llu %16llu %16llu %16llu",
+        &(cpuData->user), &(cpuData->nice), &(cpuData->system), &(cpuData->idle),
+        &(cpuData->iowait), &(cpuData->irq), &(cpuData->softirq), &(cpuData->steal),
+        &(cpuData->guest), &(cpuData->guest_nice));
+
+    return;
+}
+
 int main(){
     return 0;
 }
